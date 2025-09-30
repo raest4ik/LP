20d8cc5de3b42c09517361d6240458459189c715
raest4ik <115368229+raest4ik@users.noreply.github.com>
2025-09-30 14:05:23 +0500

    Б3

--- labaO.cpp
+++ labaO.cpp
@@ -5,37 +5,42 @@
 const int m = 10, n = 20; // m - число строк, n - число столбцов
 
 float mtx[m][n];        // матрица вещественных чисел
-float row_avgs[m];      // массив средних по строкам
 int row_numbers[m];     // номера строк 
+int col_numbers[n];     // номера столбцов
 
 // Функция потока - заполнение одной строки случайными вещественными числами
 DWORD WINAPI row_fill(LPVOID param)
 {
 
-    srand(time(0));
-    
     int* prow_num = (int*)param;
     int row = *prow_num;
 
+    srand(time(0)+row);
+
     for (int j = 0; j < n; ++j)
     {
-        mtx[row][j] = (rand()) + (float)rand()/100;
+        mtx[row][j] = (rand()) + (float)rand() / 100;
     }
     return 0;
 }
 
-// Функция потока - вычисление среднего значения по одной строке
-DWORD WINAPI row_avg(LPVOID param)
-{
-    int* prow_num = (int*)param;
-    int row = *prow_num;
 
-    float sum = 0.0f;
-    for (int j = 0; j < n; ++j)
-        sum += mtx[row][j];
-    row_avgs[row] = sum / n;
+
+DWORD WINAPI col_sort(LPVOID param)
+{
+    int col = *(int*)param;
+    for (int i = 0; i < m - 1; ++i) {
+        for (int k = i + 1; k < m; ++k) {
+            if (mtx[i][col] > mtx[k][col]) {
+                float tmp = mtx[i][col];
+                mtx[i][col] = mtx[k][col];
+                mtx[k][col] = tmp;
+            }
+        }
+    }
     return 0;
 }
+
 
 void print_m(const float a[m][n]) {
     for (int i = 0; i < m; i++) {
@@ -45,93 +50,59 @@ void print_m(const float a[m][n]) {
         std::cout << "\n";
     }
 }
-
 int main()
 {
     setlocale(LC_ALL, "rus");
     srand(static_cast<unsigned>(time(NULL)));
-
-    // Инициализация номеров строк
-    for (int i = 0; i < m; ++i) {
-        row_numbers[i] = i;
-        row_avgs[i] = 0.0f;
-    }
-
+    // Инициализация номеров строк и столбцов
+    for (int i = 0; i < m; ++i) row_numbers[i] = i;
+    for (int j = 0; j < n; ++j) col_numbers[j] = j;
     // Потоки для заполнения строк
     HANDLE fillThreads[m];
     DWORD fillIDs[m];
-
     for (int i = 0; i < m; ++i) {
         fillThreads[i] = CreateThread(
-            NULL,               // безопасность по умолчанию
-            0,                  // размер стека по умолчанию
-            row_fill,           // функция
-            &row_numbers[i],    // параметр (номер строки)
-            0,                  // флаги создания
-            &fillIDs[i]);       // id потока
-
-        // Проверям - создан ли поток
-        if (fillThreads[i] == NULL)
-        {
-            std::cout << "Поток № " << i
-                << "не был создан\n"
-                << "Ошибка "
-                << GetLastError();
-        }
-    }
-
-    // Ждём завершения всех потоков заполнения
-    WaitForMultipleObjects(m, fillThreads, TRUE, INFINITE);
-
-    // Закрытие потоков
-    for (int i = 0; i < m; ++i)
-        CloseHandle(fillThreads[i]);
-
-    std::cout << "Вывод матрицы:\n";
-    print_m(mtx);
-
-    // Потоки для вычисления среднего по строкам
-    HANDLE avgThreads[m];
-    DWORD avgIDs[m];
-
-    for (int i = 0; i < m; ++i) {
-        avgThreads[i] = CreateThread(
             NULL,
             0,
-            row_avg,
+            row_fill,
             &row_numbers[i],
             0,
-            &avgIDs[i]);
-
-        if (avgThreads[i] == NULL)
+            &fillIDs[i]);
+        if (fillThreads[i] == NULL)
         {
-            std::cout << "Поток № " << i
-                << "не был создан\n"
-                << "Ошибка "
-                << GetLastError();
+            std::cout << "Поток заполнения № " << i << " не был создан. Ошибка " << GetLastError() << "\n";
+            return 1;
         }
     }
-
-    // Ждём завершения всех потоков вычисления
-    WaitForMultipleObjects(m, avgThreads, TRUE, INFINITE);
-
-    // Закрытие потоков
-    for (int i = 0; i < m; ++i)
-        CloseHandle(avgThreads[i]);
-
-    // Находим номер строки с максимальным средним значением
-    int num_max = 0;
-    float max_avg = row_avgs[0];
-    for (int i = 1; i < m; ++i) {
-        if (row_avgs[i] > max_avg) {
-            max_avg = row_avgs[i];
-            num_max = i;
+    // Ждём завершения всех потоков заполнения
+    WaitForMultipleObjects(m, fillThreads, TRUE, INFINITE);
+    for (int i = 0; i < m; ++i) 
+        CloseHandle(fillThreads[i]);
+    std::cout << "Матрица до сортировки столбцов:\n";
+    print_m(mtx);
+    // Потоки для параллельной сортировки столбцов
+    HANDLE sortThreads[n];
+    DWORD sortIDs[n];
+    for (int j = 0; j < n; ++j) {
+        sortThreads[j] = CreateThread(
+            NULL,
+            0,
+            col_sort,
+            &col_numbers[j],
+            0,
+            &sortIDs[j]);
+        if (sortThreads[j] == NULL)
+        {
+            std::cerr << "Поток сортировки столбца № " << j << " не был создан. Ошибка " << GetLastError() << "\n";
+            // В случае ошибки закрываем уже созданные и выходим
+            for (int k = 0; k < j; ++k) CloseHandle(sortThreads[k]);
+            return 1;
         }
     }
-
-
-    std::cout << "Искомая строка с максимальным средним: № " << num_max
-        << " (" << max_avg << ")\n";
-
+    // Ждём завершения всех потоков сортировки
+    WaitForMultipleObjects(n, sortThreads, TRUE, INFINITE);
+    for (int j = 0; j < n; ++j) CloseHandle(sortThreads[j]);
+    std::cout << "\nМатрица сортировки столбцов:\n";
+    print_m(mtx);
     return 0;
 }
